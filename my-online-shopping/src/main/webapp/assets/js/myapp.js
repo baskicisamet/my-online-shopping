af0fe83259088ef5eb31cccb8079e5148a427085
sam <baskicisamet@gmail.com>
2017-09-16 03:41:43 +0300

    fixed some bugs

--- my-online-shopping/src/main/webapp/assets/js/myapp.js
+++ my-online-shopping/src/main/webapp/assets/js/myapp.js
@@ -70,15 +70,33 @@ $(function(){
 					}
 				},
 				{
-					data: 'quantity'
-				},{
+					data: 'quantity',
+					mRender:function(data, type, row){
+						
+						if(data < 1){
+							 return '<span style="color:red">Out of Stock!</span>';
+						}
+						
+						return data; 
+						
+					}
+				},
+				{
 					data: 'id',
 					bSortable:false,
 					mRender:function(data,type,row){
 						
 						var str = '';
 						str += '<a href="' + window.contextRoot+ '/show/' + data + '/product" class="btn btn-primary"><i class="fa fa-opencart" aria-hidden="true"></i></a> &#160';
-						str += '<a href="' + window.contextRoot+ '/cart/add/' + data + '/product" class="btn btn-success"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a>'; 
+						
+						if(row.quantity < 1){
+							str += '<a href="javascript:void(0)" class="btn btn-success disabled"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a>'; 
+						}else{
+							str += '<a href="' + window.contextRoot+ '/cart/add/' + data + '/product" class="btn btn-success"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a>';  
+							 
+						}
+						
+						
 						return str;
 						
 						
